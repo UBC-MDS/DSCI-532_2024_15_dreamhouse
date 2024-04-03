5f1243c94c847f0254f58739144dfe04b32254b3
Chun Li <kusha@student.ubc.ca>
2024-04-02 21:32:43 -0700

    First implementation of dashboard with main heatmap

--- src/app.py
+++ src/app.py
@@ -1,24 +1,57 @@
-from dash import Dash, html
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
 
-# Initialize the Dash app
-app = Dash(__name__)
-server = app.server
+# Initialize the Dash application
+app = dash.Dash(__name__)
 
-# Define the layout of the app
-app.layout = html.Div([
-    html.H1("American Housing Prices Dashboard"),
-    html.H2("Project Overview"),
-    html.P("This dashboard presents an interactive visualization of housing prices in top cities and states across top 50 cities in the United States of America. The data is sourced from a comprehensive dataset available on Kaggle, which includes demographics and house prices of the top cities in the United States. Our dashboard is built using the Dash package, enabling users to filter and explore the housing market data across various dimensions, such as state, city, house type, square footage, number of bedrooms and bathrooms, and more."),
-    html.H3("This dashboard is for UBC MDS Program DSCI 532"),
-    html.H4("Authors:"),
-    html.Ul([
-        html.Li("Ruocong Sun"),
-        html.Li("Chun Li"),
-        html.Li("Jake Barnabe"),
-        html.Li("Zeily Garcia")
-    ])
+# Load and prepare the dataset
+df = pd.read_csv('../data/processed/processed_df.csv')
+
+# Average Price (per sqft) by State
+state_avg_prices = df.groupby('State')['Price per SqFt'].mean().reset_index()
+
+# App layout
+app.layout = html.Div(children=[
+    html.H1(children='Dreamhouse Real Estate Dashboard'),
+    
+    dcc.Graph(
+        id='us-map',
+    ),
 ])
 
-# Run the app
+# Callback to update the heatmap
+@app.callback(
+    Output('us-map', 'figure'),
+    Input('us-map', 'id'))
+def update_map(dummy):
+    fig = px.choropleth(
+        state_avg_prices,
+        locations='State',
+        locationmode='USA-states',
+        color='Price per SqFt',
+        color_continuous_scale='Viridis',
+        scope='usa',
+        title='Average Price per SqFt by State'
+    )
+
+    # Set the size of the map
+    fig.update_layout(height=750, width=900)
+
+    # Adjust the legend position and orientation
+    fig.update_layout(
+        legend=dict(
+            x=0.5,  # Adjust the x position as needed
+            y=-0.1,  # Adjust the y position as needed
+            orientation="h"
+        )
+    )
+
+    return fig
+
+
+# Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
