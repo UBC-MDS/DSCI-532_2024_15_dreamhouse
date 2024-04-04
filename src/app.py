be49964874e231b9910b2ad6cd99e81ce3bce885
ZEILY GARCIA <zeilygarcia2017@gmail.com>
2024-04-03 20:06:55 -0700

    This commit changed the layout - a layout we could follow to avoid merge conflicts

--- src/app.py
+++ src/app.py
@@ -1,153 +1,163 @@
 import dash
 from dash import html, dcc
-#from dash_bootstrap_components as dbc
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
 # Initialize the Dash application
-app = dash.Dash(__name__) #external_stylesheets=[dbc, themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-# Load and prepare the dataset
-df = pd.read_csv('data/processed/processed_df.csv')
+df = pd.read_csv('data/processed/processed_df.csv')  
 
-# App layout
-app.layout = html.Div(children=[
-    html.H1(children='Dreamhouse Real Estate Dashboard'),
-    
-    # filterss
-    html.Div([
-        # selection state & city 
-        html.Label('State'),
-        dcc.Dropdown(
-            id='state-dropdown',
-            options=[{'label': 'All', 'value': 'All'}] +
-                    [{'label': state, 'value': state} for state in df['State'].unique()],
-            value='All',
-            clearable=False,
-        ),
-        html.Label('City'),
-        dcc.Dropdown(
-            id='city-dropdown',
-            multi=False, #This could be an improvement? -- maybe we can Add multiple cities at once -- complicates bars
-            options=[{'label': 'All', 'value': 'All'}],
-            value='All',
-            clearable=False,
-        ),
+title = dbc.Row([dbc.Col(html.H1('Dreamhouse Real Estate Dashboard'), width=12)])
 
-        # living space RangeSlider
-        html.Label('Square Footage'),
-        dcc.RangeSlider(
-            id='square-footage-slider',
-            min= df['Living Space'].min(),
-            max=df['Living Space'].max(),
-            value=[df['Living Space'].min(), df['Living Space'].max()],
-            updatemode='drag',
-            tooltip={'always_visible': False, 'placement': 'top'}, 
-        ),
-        # price RangeSlider
-        html.Label('Price Range'),
-        dcc.RangeSlider(
-            id='price-range-slider',
-            min= df['Price'].min(),
-            max=df['Price'].max(),
-            value=[df['Price'].min(), df['Price'].max()],
-            updatemode='drag',
-            tooltip={'always_visible': False, 'placement': 'top'}, 
-        ),
-        # Beds & baths
-        html.Label('Beds'),
-        dcc.RangeSlider(
-            id='beds-slider',
-            min= df['Beds'].min(),
-            max=df['Beds'].max(),
-            value=[df['Beds'].min(), df['Beds'].max()],
-            updatemode='drag',
-            tooltip={'always_visible': False, 'placement': 'top'} ,
-        ),
-        html.Label('Baths'),
-        dcc.RangeSlider(
-            id='baths-slider',
-            min= df['Baths'].min(),
-            max=df['Baths'].max(),
-            value=[df['Baths'].min(), df['Baths'].max()],
-            updatemode='drag',
-            tooltip={'always_visible': False, 'placement': 'top'},
-        ),
-    ], style={'columnCount': 2}),
-    
-    # graph to be colored by state
-    dcc.Graph(
-        id='city-bar-graph',
-    ),
-    
-])
+state_dropdown = dcc.Dropdown(
+    id='state-dropdown',
+    options=[{'label': 'All', 'value': 'All'}] + [{'label': state, 'value': state} for state in df['State'].unique()],
+    value='All',
+    clearable=False
+)
 
-# state-dropdown --> updates the city-dropdown options
+city_dropdown = dcc.Dropdown(
+    id='city-dropdown',
+    options=[{'label': 'All', 'value': 'All'}],
+    value='All',
+    clearable=False,
+)
+
+square_footage_slider = dcc.RangeSlider(
+    id='square-footage-slider',
+    min=df['Living Space'].min(),
+    max=df['Living Space'].max(),
+    value=[df['Living Space'].min(), df['Living Space'].max()],
+    tooltip={'placement': 'bottom', 'always_visible': False}
+)
+
+price_range_slider = dcc.RangeSlider(
+    id='price-range-slider',
+    min=df['Price'].min(),
+    max=df['Price'].max(),
+    value=[df['Price'].min(), df['Price'].max()],
+    tooltip={'placement': 'bottom', 'always_visible': False}
+)
+
+beds_slider = dcc.RangeSlider(
+    id='beds-slider',
+    min=df['Beds'].min(),
+    max=df['Beds'].max(),
+    value=[df['Beds'].min(), df['Beds'].max()],
+    tooltip={'placement': 'bottom', 'always_visible': False}
+)
+
+baths_slider = dcc.RangeSlider(
+    id='baths-slider',
+    min=df['Baths'].min(),
+    max=df['Baths'].max(),
+    value=[df['Baths'].min(), df['Baths'].max()],
+    tooltip={'placement': 'bottom', 'always_visible': False}
+)
+
+city_bar_graph = dcc.Graph(id='city-bar-graph')
+
+app.layout = dbc.Container([
+title,
+    
+    # Main Row
+    dbc.Row([
+        # first column --> filters
+        dbc.Col([
+            dbc.Label('State'),
+            state_dropdown, 
+            html.Br(),
+            dbc.Label('City'),
+            city_dropdown,  
+            html.Br(),
+            dbc.Label('Square Footage'),
+            square_footage_slider, 
+            html.Br(),
+            dbc.Label('Price Range'),
+            price_range_slider, 
+            html.Br(),
+            dbc.Label('Beds'),
+            beds_slider, 
+            html.Br(),
+            dbc.Label('Baths'),
+            baths_slider, 
+        ], md=2),
+
+        # Second column is for -->  Map, bar, and summary Stats
+        dbc.Col([
+            # Row for map and bar
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label('Map')  #Placeholder
+                ], md=6),
+                dbc.Col([
+                    city_bar_graph 
+                ], md=6),
+            ]),
+            
+            # row for summary statistics -- this row could have two rows and 3 columns inside? --> this depends on how many summary stats we end up with
+            dbc.Row([
+                dbc.Col([
+                    dbc.Label('Summary statistics')  #placeholder
+                ])
+            ])
+        ], md=10),
+    ])
+], fluid=True)
+
+
 @app.callback(
     Output('city-dropdown', 'options'),
     Input('state-dropdown', 'value'))
-
 def set_cities_options(selected_state):
     if selected_state == 'All':
-        return [{'label': 'All', 'value': 'All'}] + [
-            {'label': city, 'value': city} for city in df['City'].unique()
-        ]
+        return [{'label': 'All', 'value': 'All'}] + [{'label': city, 'value': city} for city in df['City'].unique()]
     else:
-        return [{'label': 'All', 'value': 'All'}] + [
-            {'label': city, 'value': city} for city in df[df['State'] == selected_state]['City'].unique()
-        ]
+        return [{'label': 'All', 'value': 'All'}] + [{'label': city, 'value': city} for city in df[df['State'] == selected_state]['City'].unique()]
 
-# update the city bar graph
+
 @app.callback(
     Output('city-bar-graph', 'figure'),
-    Input('state-dropdown', 'value'),
-    Input('city-dropdown', 'value'),
-    Input('square-footage-slider', 'value'),
-    Input('price-range-slider', 'value'),
-    Input('beds-slider', 'value'),
-    Input('baths-slider', 'value'))
-
-
+    [Input('state-dropdown', 'value'),
+     Input('city-dropdown', 'value'),
+     Input('square-footage-slider', 'value'),
+     Input('price-range-slider', 'value'),
+     Input('beds-slider', 'value'),
+     Input('baths-slider', 'value')])
 def update_city_bar_graph(state, city, square_footage_range, price_range, beds, baths):
     filtered_df = df.copy()
-
-    # State filter
+    
     if state != 'All':
         filtered_df = filtered_df[filtered_df['State'] == state]
-
-    # City filter
+    
     if city != 'All':
         filtered_df = filtered_df[filtered_df['City'] == city]
-
-    # living space
+    
     min_sqft, max_sqft = square_footage_range
     filtered_df = filtered_df[(filtered_df['Living Space'] >= min_sqft) & (filtered_df['Living Space'] <= max_sqft)]
-
-    # price 
+    
     min_price, max_price = price_range
     filtered_df = filtered_df[(filtered_df['Price'] >= min_price) & (filtered_df['Price'] <= max_price)]
-
-    # Beds & baths
+    
     min_beds, max_beds = beds
     filtered_df = filtered_df[(filtered_df['Beds'] >= min_beds) & (filtered_df['Beds'] <= max_beds)]
     
     min_baths, max_baths = baths
     filtered_df = filtered_df[(filtered_df['Baths'] >= min_baths) & (filtered_df['Baths'] <= max_baths)]
-
-    # when data is empty, display no data tp plot
+    
     if not filtered_df.empty:
         city_avg_prices = filtered_df.groupby(['City', 'State'], as_index=False)['Price'].mean()
-
         fig = px.bar(
             city_avg_prices,
             y='City',
-            x='Price',        #Missing count! Unsure if this is the only way to hover?
+            x='Price',
             color='State',
             title='Average House Pricing by City'
         )
-
         fig.update_layout(
             yaxis={'categoryorder': 'total descending'},
             xaxis_title='Average Price',
@@ -161,7 +171,7 @@ def update_city_bar_graph(state, city, square_footage_range, price_range, beds, 
             xaxis={'visible': False},
             yaxis={'visible': False}
         )
-
+    
     return fig
 
 if __name__ == '__main__':
