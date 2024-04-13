ddd7063376fd3d89d565d00b44f1595787ba91fe
Chun Li <kusha@student.ubc.ca>
2024-04-13 14:01:25 -0700

    implemented county level fibs successfully

--- src/figures.py
+++ src/figures.py
@@ -1,52 +1,38 @@
-
 import plotly.express as px
 from src.data import state_avg_prices
-from src.data import df
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
 
+def generate_us_map(data, geojson, location_field, scope='usa'):
+    """
+    Generate a US choropleth map based on the provided data.
 
-def generate_us_map(selected_state=None):
-    if selected_state:
-        # Filter data for the selected state and generate a county-level choropleth
-        filtered_df = df[df['code'] == selected_state]
-        fig = px.choropleth(
-            filtered_df, 
-            geojson=counties, 
-            locations='fips', 
-            color='Price per SqFt',
-            color_continuous_scale="Viridis",
-            scope="usa"
-        )
-    else:
-        # Generate the default US map showing state averages
-        fig = px.choropleth(
-            state_avg_prices,
-            locations='code',
-            locationmode="USA-states",
-            color='Price per SqFt',
-            scope='usa'
-        )
-    fig.update_layout(title_text='US Choropleth Map', geo_scope='usa')
+    Args:
+    data (DataFrame): Data to display on the map, with necessary columns for locations and values.
+    geojson (dict): GeoJSON data for the map outlines.
+    location_field (str): The DataFrame column to be used as the identifier in the geojson mapping.
+    scope (str, optional): The geographic scope of the map, defaults to 'usa'.
+
+    Returns:
+    Figure: A Plotly figure object for the choropleth map.
+    """
+    fig = px.choropleth(
+        data,
+        geojson=geojson,
+        locations=location_field,
+        color='Price per SqFt',
+        color_continuous_scale="Viridis",
+        scope=scope
+    )
+    fig.update_geos(fitbounds="locations", visible=True)
+    fig.update_layout(title_text='US Property Prices Overview', geo_scope=scope)
     return fig
 
-def generate_city_map(state_df):
-    fig = px.scatter_geo(
-        state_df,
-        locations='fips',
+def generate_default_map():
+    fig = px.choropleth(
+        state_avg_prices,
+        locations='code',
         locationmode="USA-states",
-        lat='Latitude',
-        lon='Longitude',
         color='Price per SqFt',
-        hover_name='City',
-        scope='usa',
-        color_continuous_scale="Viridis"
+        scope='usa'
     )
-    fig.update_geos(fitbounds="locations")
-    fig.update_layout(
-        title_text='City Details',
-        geo_scope='usa'
-    )
+    fig.update_layout(title_text='National Property Prices Overview', geo_scope='usa')
     return fig
