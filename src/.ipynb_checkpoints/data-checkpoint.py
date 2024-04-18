56a4e7a041d18184df4840a96c0bdf40609d001c
sungg888 <42681419+sungg888@users.noreply.github.com>
2024-04-17 21:03:43 -0700

    remove decimal and center summary (#65)

--- src/.ipynb_checkpoints/data-checkpoint.py
+++ src/.ipynb_checkpoints/data-checkpoint.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
-def load_data():
-    return pd.read_csv('data/processed/processed_df.csv')
+def load_data(url):
+    return pd.read_csv(url)
+
+def load_feather(url):
+    return pd.read_feather(url)
+
 
 # Item associated with mouse clicking function
 state_abbreviations = {
@@ -18,6 +22,25 @@ state_abbreviations = {
 # Item associated with mouse clicking function
 state_mapping = {abbr: state for state, abbr in state_abbreviations.items()}
 
-df = load_data()
+df1_url = 'data/processed/data.feather'
+df2_url = 'https://query.data.world/s/npaool4liamcv2ltc6jeji3pq7gptl?dws=00000'
 
+df = load_feather(df1_url)
+df2 = load_data(df2_url)
+
+df['Zip Code'] = df['Zip Code'].astype(str)
+df2['ZIP'] = df2['ZIP'].astype(str)
+
+
+# State Average Price Level
+# This object is used to render the default map
 state_avg_prices = df.groupby('code')['Price per SqFt'].mean().reset_index()
+
+# The following process adds the Federal Information Processing Standards information to dataframe
+# Fibs information imported from data.world
+df = pd.merge(df, df2[['ZIP', 'STCOUNTYFP']], left_on='Zip Code', right_on='ZIP', how='left')
+df.rename(columns={'STCOUNTYFP': 'fips'}, inplace=True)
+df.drop(columns='ZIP', inplace=True)
+
+# Pad counties that have a 4 digit fips code to make them 5 digits
+df['fips_str'] = df['fips'].astype(str).str.zfill(5)
--- src/app.py
+++ src/app.py
@@ -19,3 +19,5 @@ if __name__ == '__main__':
     app.run_server(debug=True)
 
 
+
+
